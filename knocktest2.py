5e0561736fa09b2b31680c7c56a6f1ac0c629c77
Zhaojin Chu <zhaojin.chu07@gmail.com>
2026-01-17 22:49:27 +0000

    changes

--- knocktest2.py
+++
@@ -1,8 +0,0 @@
-from gpiozero import Button
-from time import sleep
-
-knock = Button(17, pull_up=True)  # LM393 OUT1 -> GPIO17, GND -> GND
-
-while True:
-    print(int(knock.value))  # 1 = idle, 0 = knock (active-low)
-    sleep(0.1)
